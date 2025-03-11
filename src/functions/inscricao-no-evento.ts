a797852b5f31b7487d76ae359f22cd4de50bf87e
Neemias Cormino Manso <130704790+n33miaz@users.noreply.github.com>
2025-03-11 17:13:31 -0300

    add: DrizzleError

--- src/functions/inscricao-no-evento.ts
+++ src/functions/inscricao-no-evento.ts
@@ -1,8 +1,9 @@
 import { eq } from 'drizzle-orm'
-import { PostgresError } from 'postgres'
 import { db } from '../drizzle/client'
 import { inscricoes } from '../drizzle/tables/inscricoes'
 import { redis } from '../redis/client'
+import { DrizzleError } from 'drizzle-orm';
+
 
 interface inscricaoNoEventoParams {
   nome: string
@@ -15,8 +16,8 @@ export async function inscricaoNoEvento({
   email,
   referenciadorId,
 }: inscricaoNoEventoParams) {
-  // busca no banco de dados algum acesso anterior
   try {
+    // busca no banco de dados algum acesso anterior
     const inscritos = await db
       .select()
       .from(inscricoes)
@@ -34,33 +35,36 @@ export async function inscricaoNoEvento({
       })
       .returning()
 
+
     if (referenciadorId) {
-      // pontua no ranking quem convidou
-      await redis.zincrby('referencia:ranking', 1, referenciadorId) // sorted sets
-    }
+        // verifica se o referenciador existe antes de incrementar
+        const referenciadorExists = await db.select().from(inscricoes).where(eq(inscricoes.id, referenciadorId));
+          if(referenciadorExists.length > 0){
+            // pontua no ranking quem convidou
+            await redis.zincrby('referencia:ranking', 1, referenciadorId);
+          }
+      }
+
 
     const inscrito = resultado[0]
     return {
       inscritoId: inscrito.id,
     }
   } catch (error) {
-    if (error instanceof PostgresError) {
-      // trata erros específicos do Postgres (ex: violação de chave única, problema de conexão)
-      console.error(
-        'Erro no Postgres:',
-        error.message,
-        error.code,
-        error.detail
-      )
-      throw new Error('Erro ao inserir inscrição no banco de dados.') // lança um erro genérico
-    } else if (error instanceof Error && error.message.startsWith('ERR')) {
-      // trata erros do Redis
-      console.error('Erro no Redis:', error.message)
-      throw new Error('Erro ao atualizar ranking no Redis.')
+    // usar o tratamento de erros do proprio Drizzle.
+    if (error instanceof DrizzleError) {
+        console.error("Erro no Drizzle:", error.message);
+        throw new Error("Erro ao inserir inscrição no banco de dados."); // erro genérico
+
+    }else if (error instanceof Error && error.message.startsWith("ERR")) {
+        //Trata erros do Redis
+        console.error("Erro no Redis:", error.message);
+        throw new Error("Erro ao atualizar ranking no Redis.");
+
     } else {
       // outros erros (incluindo erros de rede com Redis)
-      console.error('Erro inesperado:', error)
-      throw new Error('Erro inesperado ao processar a inscrição.') // lança um erro genérico
+      console.error("Erro inesperado:", error);
+      throw new Error("Erro inesperado ao processar a inscrição."); // lança um erro genérico
     }
   }
 }
