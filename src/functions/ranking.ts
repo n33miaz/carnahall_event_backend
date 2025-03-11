63875684a3e353e8360e31e9e24cddc8455af82e
Neemias Cormino Manso <130704790+n33miaz@users.noreply.github.com>
2025-03-11 16:56:24 -0300

    remove: sql<number>...

--- src/functions/ranking.ts
+++ src/functions/ranking.ts
@@ -1,4 +1,4 @@
-import { inArray, sql } from 'drizzle-orm' 
+import { inArray } from 'drizzle-orm'
 import { db } from '../drizzle/client'
 import { inscricoes } from '../drizzle/tables/inscricoes'
 import { redis } from '../redis/client'
@@ -13,10 +13,10 @@ export async function ranking() {
   )
 
   if (rankingData.length === 0) {
-    return { rankingComPontuacao: [] } // retorna um array vazio se não houver ranking
+    return { rankingComPontuacao: [] } // retorna um array vazio se não houver ranking.
   }
 
-  // converter o resultado do Redis em um formato mais utilizável 
+  // converter o resultado do Redis em um formato mais utilizável (array de objetos).
   const ranking: { id: string; pontuacao: number }[] = []
   for (let i = 0; i < rankingData.length; i += 2) {
     const id = rankingData[i]
@@ -34,12 +34,11 @@ export async function ranking() {
     ranking.push({ id, pontuacao: score })
   }
 
-  // buscar os dados dos inscritos no banco de dados, usando os IDs do Redis.
+  // buscar os dados dos inscritos no banco de dados, usando os IDs do Redis
   const inscritos = await db
     .select({
       id: inscricoes.id,
       nome: inscricoes.nome,
-      // pontuacao: sql<number>`COALESCE(${redis}.zscore('referencia:ranking', ${inscricoes.id}), 0)`
     })
     .from(inscricoes)
     .where(
@@ -47,19 +46,18 @@ export async function ranking() {
         inscricoes.id,
         ranking.map(r => r.id)
       )
-    ) // IDs do ranking
+    ) // usamos os IDs do ranking
 
-  // combinar os dados do Redis com os dados do banco de dados
-  // a ordenação é feita com base no 'ranking' que veio do redis
-  const rankingComPontuacao = ranking.map(rankItem => {
-    const inscrito = inscritos.find(i => i.id === rankItem.id)
-    // se o inscrito não for encontrado
-    return {
-      id: rankItem.id,
-      nome: inscrito ? inscrito.nome : 'Nome não encontrado', // trata caso o inscrito não seja encontrado
-      pontuacao: rankItem.pontuacao, // pega a pontuação que já veio do Redis
-    }
-  })
+    const rankingComPontuacao = ranking.map(rankItem => {
+        const inscrito = inscritos.find(i => i.id === rankItem.id);
+        // se o inscrito não for encontrado
+        return {
+          id: rankItem.id,
+          nome: inscrito ? inscrito.nome : 'Nome não encontrado', // trata caso o inscrito não seja encontrado
+          pontuacao: rankItem.pontuacao, // pega a pontuação que já veio do Redis
+        };
+    });
+  
 
   return { rankingComPontuacao }
 }
